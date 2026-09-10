d3487701d7e078331fe184362ea913c97641c1c5
PeyMuz <kbantog1@gmail.com>
2026-09-10 22:00:22 +0800

    Finished

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const generateBtn = document.getElementById("generate-btn");
 const palleteContainer = document.querySelector(".palette-container");
+const copyBtn = document.querySelector(".copy-btn");
+
 
 generateBtn.addEventListener("click", generatePallete);
 
+//to copy the hexValue
 palleteContainer.addEventListener("click", function(e)  {
     if(e.target.classList.contains("copy-btn")){
+        const hexValue = e.target.previousElementSibling.textContent;
         
+        //to the text if its copied succesfully
+        navigator.clipboard.writeText(hexValue)
+        .then(() => showCopySuccess(e.target)) 
+        .catch((error) => console.log(error));
+    }else if(e.target.classList.contains("color")){
+       const hexValue = e.target.nextElementSibling.querySelector(".hex-value").textContent;
+       navigator.clipboard
+        .writeText(hexValue)
+        .then(() => showCopySuccess(e.target.nextElementSibling.querySelector(".copy-btn"))) // kapag ccinlick mo yung box color, cococpy mo parin yung hex value.
+        .catch((error) => console.log(error));        
     }
-})
+});
+
+//this function will display the success copied
+function showCopySuccess(copyBtn){
+   copyBtn.classList.remove("far","fa-copy"); //to remove
+   copyBtn.classList.add("fas","fa-check"); // to add
+
+   copyBtn.style.color = "#48bb78";
+
+   // after clicking, it will go back to the previous state
+   setTimeout(() => {
+    copyBtn.classList.remove("fas", "fa-check");
+    copyBtn.classList.add("far", "fa-copy");
+    copyBtn.style.color="";
+   }, 1500);
+
+}
 
 function generatePallete() {
 
